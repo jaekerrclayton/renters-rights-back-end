64e78a602cf474002e183c7cc7f37282d902ae0f
Christina Webber <chrisswebb@yahoo.com>
2022-07-27 10:30:03 -0500

    able to make volunteers db

--- src/main/java/com/renters_right_backend/renters_demo/controllers/VolunteerController.java
+++ src/main/java/com/renters_right_backend/renters_demo/controllers/VolunteerController.java
@@ -6,21 +6,23 @@ import java.util.List;
 import com.renters_right_backend.renters_demo.model.Volunteer;
 import com.renters_right_backend.renters_demo.repositories.VolunteerRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class VolunteerController {
+    @Autowired
+    private VolunteerRepository volunteerRepo;
 
-    private final VolunteerRepository repository;
+    @GetMapping("/volunteers")
+    public String listAll(Model model) {
+        List<Volunteer> listVolunteers = volunteerRepo.findAll() ;
+        model.addAttribute("listVolunteers", listVolunteers);
 
-    VolunteerController(VolunteerRepository repository){
-        this.repository = repository;
-    }
-
-    @GetMapping(path="/volunteers")
-    List<Volunteer> all(){
-        return repository.findAll();
+        return "volunteers";
     }
 
     

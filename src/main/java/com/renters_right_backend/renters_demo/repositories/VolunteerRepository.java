7f44928f16caaced7f8141643efd90bb22ad8df5
jae <j.kerr.clayton@gmail.com>
2022-07-27 03:15:16 -0500

    new branch psql connect

--- src/main/java/com/renters_right_backend/renters_demo/repositories/VolunteerRepository.java
+++ src/main/java/com/renters_right_backend/renters_demo/repositories/VolunteerRepository.java
@@ -3,7 +3,9 @@ package com.renters_right_backend.renters_demo.repositories;
 import com.renters_right_backend.renters_demo.model.Volunteer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VolunteerRepository extends JpaRepository<Volunteer, Integer>{
     
 }

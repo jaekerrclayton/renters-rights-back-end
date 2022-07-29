0f054b8738236141586b0e17a16e6d578177f938
Christina Webber <chrisswebb@yahoo.com>
2022-07-29 11:41:03 -0500

    matches current table

--- src/main/java/com/renters_right_backend/renters_demo/model/Volunteer.java
+++ src/main/java/com/renters_right_backend/renters_demo/model/Volunteer.java
@@ -1,11 +1,11 @@
 package com.renters_right_backend.renters_demo.model;
 
-
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,147 +14,74 @@ import javax.persistence.Table;
 public class Volunteer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer volunteerId;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
     @Column(name = "name", nullable=false)
     private String name;
 
-
-    @Column(name = "email", nullable = false)
+    @NotEmpty(message = "The email address is required")
+    @Email(message = "The email address is ivalid.")
     private String email; 
 
-    @Column(name = "schedule")
     private String schedule;
-
-
-    @Column(name = "status")
     private String status; 
-
-
-    @Column(name = "language")
     private String language; 
-
-
-      // **** SOME WAY TO SRT BY TYPE : maybe list?? 
-    @Column(name = "type")
     private String type;
 
+
     public Volunteer(){};
-  
-
-
-
 
     public Volunteer(
-        Integer volunteerId, 
+        Integer id, 
         String name, 
         String type, 
         String email, 
         String schedule, 
         String status, 
         String language)
-        
         {
-
-            this.volunteerId = volunteerId;
+            this.id = id;
             this.name = name; 
             this.type = type;
             this.email= email;
             this.schedule = schedule;
             this.status  = status;
             this.language = language;
-
         }
 
 
-
-
     public Integer getVolunteerId(){
-        return volunteerId;
+        return id;
     }
-
     public String getName(){
         return name;
     }
-
-
     public String getType(){
         return type;
     }
-
-
     public String getEmail(){
         return email;
     }
-
-
     public String getLanguage(){
         return language;
     }
-
-
     public String getSchedule(){
         return schedule;
     }
-
     public void setSchedule(String schedule){
         this.schedule = schedule;
     }
-
-
     public String getStatus(){
         return status;
     }
-
     public void setStatus(String status){
         this.status = status;
     }
 
-// do we need this ??
-
     @Override
     public String toString() {
-        return "Volunteer [volunteerId=" + volunteerId + ", name=" + name + ", email=" + email
+        return "Volunteer [id=" + id + ", name=" + name + ", email=" + email
                 + ", status=" + status + ", schedule=" + schedule + ", type=" + type + ", language=" + language + "]";
     }
-
-
 }
-
-
-
-
-
-
-
-
-    
-
-
-    
-
-
-
-   /*
-   sched times : morn afternoon evening 
-   day: m, t, w, tr, fr, sat, sun 
-   
-   {day : shift}*/
-
-
-    
-
-//    @Override
-//     public int hashCode() {
-//         int hash = 7;
-//         hash = 79 * hash + Objects.hashCode(this.id);
-//         hash = 79 * hash + Objects.hashCode(this.name);
-//         hash = 79 * hash + this.population;
-//         return hash;
-//     }
-
-
-
-    
-

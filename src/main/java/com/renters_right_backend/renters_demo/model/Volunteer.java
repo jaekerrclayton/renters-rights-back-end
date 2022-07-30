c8cd84d58e989f718254e0bdb1cfc2286e2731d3
Christina Webber <chrisswebb@yahoo.com>
2022-07-30 15:35:50 -0500

    removed extra import

--- src/main/java/com/renters_right_backend/renters_demo/model/Volunteer.java
+++ src/main/java/com/renters_right_backend/renters_demo/model/Volunteer.java
@@ -1,6 +1,5 @@
 package com.renters_right_backend.renters_demo.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;

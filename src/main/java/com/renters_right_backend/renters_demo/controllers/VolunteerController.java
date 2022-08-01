a1151c6f5999a79875435de1e9313db7d1449a4a
Christina Webber <chrisswebb@yahoo.com>
2022-08-01 15:11:10 -0500

    CORS added?

--- src/main/java/com/renters_right_backend/renters_demo/controllers/VolunteerController.java
+++ src/main/java/com/renters_right_backend/renters_demo/controllers/VolunteerController.java
@@ -12,6 +12,7 @@ import com.renters_right_backend.renters_demo.repositories.VolunteerRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/admin")
 public class VolunteerController {
 
